dcf1c68901cfc31fba51aacebf0ac3181a82ffbe
seedhammer <gh@seedhammer.com>
2024-01-05 17:08:11 -0600

    cmd/bitmapfont: turn bitmap font generator into a standalone command

--- font/comfortaa/gen.go
+++ font/comfortaa/gen.go
@@ -1,4 +1,4 @@
 package comfortaa
 
-//go:generate go run ../bitmap/convert.go -package comfortaa -ppem 17 Comfortaa-Bold.ttf bold
-//go:generate go run ../bitmap/convert.go -package comfortaa -ppem 16 Comfortaa-Regular.ttf regular
+//go:generate go run seedhammer.com/cmd/bitmapfont -package comfortaa -ppem 17 Comfortaa-Bold.ttf bold
+//go:generate go run seedhammer.com/cmd/bitmapfont -package comfortaa -ppem 16 Comfortaa-Regular.ttf regular
