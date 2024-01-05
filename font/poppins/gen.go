7ecf1dc4605df6989377fb998c57b30b925bed40
seedhammer <gh@seedhammer.com>
2024-01-05 17:08:11 -0600

    cmd/bitmapfont: turn bitmap font generator into a standalone command

--- font/poppins/gen.go
+++ font/poppins/gen.go
@@ -1,10 +1,10 @@
 package poppins
 
-//go:generate go run ../bitmap/convert.go -package poppins -ppem 16 Poppins-Regular.ttf regular
+//go:generate go run seedhammer.com/cmd/bitmapfont -package poppins -ppem 16 Poppins-Regular.ttf regular
 
-//go:generate go run ../bitmap/convert.go -package poppins -ppem 10 Poppins-Bold.ttf bold
-//go:generate go run ../bitmap/convert.go -package poppins -ppem 16 Poppins-Bold.ttf bold
-//go:generate go run ../bitmap/convert.go -package poppins -ppem 20 Poppins-Bold.ttf bold
-//go:generate go run ../bitmap/convert.go -package poppins -ppem 23 Poppins-Bold.ttf bold
+//go:generate go run seedhammer.com/cmd/bitmapfont -package poppins -ppem 10 Poppins-Bold.ttf bold
+//go:generate go run seedhammer.com/cmd/bitmapfont -package poppins -ppem 16 Poppins-Bold.ttf bold
+//go:generate go run seedhammer.com/cmd/bitmapfont -package poppins -ppem 20 Poppins-Bold.ttf bold
+//go:generate go run seedhammer.com/cmd/bitmapfont -package poppins -ppem 23 Poppins-Bold.ttf bold
 // Size 45 is only for progress indicators
-//go:generate go run ../bitmap/convert.go -package poppins -ppem 45 -alphabet "0123456789%" Poppins-Bold.ttf boldprogress
+//go:generate go run seedhammer.com/cmd/bitmapfont -package poppins -ppem 45 -alphabet "0123456789:" Poppins-Bold.ttf boldprogress
