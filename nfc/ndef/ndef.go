1b84a876fed48cb23ec5efdec77a74f889067e55
seedhammer <gh@seedhammer.com>
2025-07-12 10:09:35 +0200

    nfc/ndef: improve parsing, add tests

--- nfc/ndef/ndef.go
+++ nfc/ndef/ndef.go
@@ -10,7 +10,7 @@ import (
 
 type Reader struct {
 	r       *bufio.Reader
-	scratch [2]byte
+	scratch [4]byte
 }
 
 func NewReader(rd io.Reader) *Reader {
@@ -21,95 +21,183 @@ func NewReader(rd io.Reader) *Reader {
 	}
 }
 
+// Read the next NDEF record, or [io.EOF] if no more records
+// are available.
 func (r *Reader) Read(buf []byte) (int, error) {
-	// Read until a NDEF message is reached.
-	var length int
+	bo := binary.BigEndian
 	for {
-		typ, err := r.r.ReadByte()
-		if err != nil {
+		typLen := r.scratch[:2]
+		if _, err := io.ReadFull(r.r, typLen); err != nil {
+			if err == io.EOF {
+				return 0, io.EOF
+			}
 			return 0, fmt.Errorf("ndef: %w", err)
 		}
-		length8, err := r.r.ReadByte()
-		if err != nil {
-			return 0, fmt.Errorf("ndef: %w", err)
-		}
-		length = int(length8)
+		// Read type and length.
+		typ, length8 := typLen[0], typLen[1]
+		length := int(length8)
 		if length8 == 0xff {
 			// 2-byte length.
 			l16 := r.scratch[:2]
 			if _, err := io.ReadFull(r.r, l16); err != nil {
 				return 0, fmt.Errorf("ndef: %w", err)
 			}
-			length = int(binary.BigEndian.Uint16(l16))
+			length = int(bo.Uint16(l16))
 		}
-		if typ == ndefType {
-			break
+		// Skip non-NDEF containers.
+		if typ != ndefType {
+			if _, err := r.r.Discard(length); err != nil {
+				return 0, fmt.Errorf("ndef: %w", err)
+			}
+			continue
 		}
-		// Skip record.
-		if _, err := r.r.Discard(length); err != nil {
-			return 0, fmt.Errorf("ndef: %w", err)
+		var discard int
+		for {
+			if discard > 0 {
+				if _, err := r.r.Discard(discard); err != nil {
+					return 0, fmt.Errorf("ndef: %w", err)
+				}
+				discard = 0
+			}
+			// Read the header and type length.
+			h := r.scratch[:2]
+			if length < len(h) {
+				return 0, fmt.Errorf("ndef: record too short")
+			}
+			if _, err := io.ReadFull(r.r, h); err != nil {
+				return 0, fmt.Errorf("ndef: %w", err)
+			}
+			flags, tlen := h[0], h[1]
+			var plen int
+			// Read payload length.
+			if flags&flagSR == 0 {
+				// 32-bit length.
+				b := r.scratch[:4]
+				if _, err := io.ReadFull(r.r, b); err != nil {
+					return 0, fmt.Errorf("ndef: %w", err)
+				}
+				plen = int(bo.Uint32(b))
+			} else {
+				// Short record.
+				l, err := r.r.ReadByte()
+				if err != nil {
+					return 0, fmt.Errorf("ndef: %w", err)
+				}
+				plen = int(l)
+			}
+			// Read ID length.
+			var idLen uint8
+			if flags&flagIR != 0 {
+				l, err := r.r.ReadByte()
+				if err != nil {
+					return 0, fmt.Errorf("ndef: %w", err)
+				}
+				idLen = l
+			}
+			// Read the well-known type byte, if any.
+			var wellKnown byte
+			if tlen == 1 {
+				t, err := r.r.ReadByte()
+				if err != nil {
+					return 0, fmt.Errorf("ndef: %w", err)
+				}
+				tlen--
+				wellKnown = t
+			}
+			// Skip the (remaining) type and id.
+			if _, err := r.r.Discard(int(tlen) + int(idLen)); err != nil {
+				return 0, fmt.Errorf("ndef: %w", err)
+			}
+			// Reject chunked records.
+			if flags&flagCF != 0 {
+				discard = plen
+				continue
+			}
+			// Skip unknown formats.
+			switch tnf := flags & 0b111; tnf {
+			case tnfWellKnown:
+			default:
+				discard = plen
+				continue
+			}
+			switch wellKnown {
+			case 'T': // Text
+				header, err := r.r.ReadByte()
+				if err != nil {
+					return 0, fmt.Errorf("ndef: %w", err)
+				}
+				plen--
+				if header&(0b1<<7) != 0 { // Don't bother with UTF-16.
+					discard = plen
+					continue
+				}
+				// Skip language.
+				langLen := int(header & 0b111111)
+				if langLen > plen {
+					return 0, errors.New("ndef: text language too long")
+				}
+				if _, err := r.r.Discard(int(langLen)); err != nil {
+					return 0, fmt.Errorf("ndef: %w", err)
+				}
+				plen := plen - langLen
+				rem := min(int(plen), len(buf))
+				n, err := io.ReadFull(r.r, buf[:rem])
+				if err != nil {
+					return n, fmt.Errorf("ndef: %w", err)
+				}
+				if n < plen {
+					return n, io.ErrShortBuffer
+				}
+				return n, io.EOF
+			case 'U': // URI
+				header, err := r.r.ReadByte()
+				if err != nil {
+					return 0, fmt.Errorf("ndef: %w", err)
+				}
+				plen--
+				prefix := ""
+				switch p := header; p {
+				case uriPrefixNone:
+				case uriPrefixHttpWww:
+					prefix = "http://www."
+				case uriPrefixHttpsWww:
+					prefix = "https://www."
+				case uriPrefixHttp:
+					prefix = "http://"
+				case uriPrefixHttps:
+					prefix = "https://"
+				default:
+					discard = plen
+					continue
+				}
+				n := copy(buf, prefix)
+				buf = buf[n:]
+				rem := min(int(plen), len(buf))
+				pn, err := io.ReadFull(r.r, buf[:rem])
+				n += pn
+				if err != nil {
+					return n, fmt.Errorf("ndef: %w", err)
+				}
+				if n < int(plen)+len(prefix) {
+					return n, io.ErrShortBuffer
+				}
+				return n, io.EOF
+			}
+			discard = plen
 		}
 	}
-	if length < 6 {
-		return 0, fmt.Errorf("ndef: record too short")
-	}
-	if len(buf) < length {
-		return 0, io.ErrShortBuffer
-	}
-	msg := buf[:length]
-	if _, err := io.ReadFull(r.r, msg); err != nil {
-		return 0, fmt.Errorf("ndef: %w", err)
-	}
-	header, tlen, plen := msg[0], msg[1], msg[2]
-	if header != 0b11010_001 || tlen != 1 { // TODO: do better
-		return 0, errors.New("ndef: unsupported ndef header")
-	}
-	payload := msg[4 : 4+plen]
-	switch mimeType := msg[3]; mimeType {
-	case 'T':
-		header := payload[0]
-		if header&(0b1<<7) != 0 { // Don't bother with UTF-16.
-			return 0, errors.New("ndef: unsupported text encoding")
-		}
-		payload = payload[1:]
-		// Skip language.
-		langLen := int(header & 0b111111)
-		if langLen > len(payload) {
-			return 0, errors.New("ndef: text language too long")
-		}
-		payload = payload[langLen:]
-		copy(buf, payload)
-		return len(payload), io.EOF
-	case 'U':
-		// URI.
-		prefix := ""
-		switch p := payload[0]; p {
-		case uriPrefixNone:
-		case uriPrefixHttpWww:
-			prefix = "http://www."
-		case uriPrefixHttpsWww:
-			prefix = "https://www."
-		case uriPrefixHttp:
-			prefix = "http://"
-		case uriPrefixHttps:
-			prefix = "https://"
-		default:
-			return 0, errors.New("ndef: unsupported URI prefix")
-		}
-		payload = payload[1:]
-		n := len(payload) + len(prefix)
-		if len(buf) < n {
-			return 0, io.ErrShortBuffer
-		}
-		copy(buf[len(prefix):], payload)
-		copy(buf, prefix)
-		return n, io.EOF
-	}
-	return 0, errors.New("ndef: unsupported payload type")
 }
 
 const (
 	ndefType = 0x03
+
+	flagIR = 0b1 << 3
+	flagSR = 0b1 << 4
+	flagCF = 0b1 << 5
+	flagME = 0b1 << 6
+	flagMB = 0b1 << 7
+
+	tnfWellKnown = 0x01
 
 	uriPrefixNone     = 0x00
 	uriPrefixHttpWww  = 0x01
