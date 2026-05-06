edcf66dd130452994d1fc48be925366234303c20
seedhammer <gh@seedhammer.com>
2026-05-06 15:32:51 +0200

    gui/op: add golden image tests

--- gui/op/draw_test.go
+++ gui/op/draw_test.go
@@ -1,13 +1,26 @@
 package op
 
 import (
+	"bytes"
+	"flag"
+	"fmt"
 	"image"
 	"image/color"
+	"image/png"
+	"os"
+	"path/filepath"
 	"slices"
 	"testing"
 
+	"seedhammer.com/font/poppins"
+	"seedhammer.com/gui/assets"
 	"seedhammer.com/image/alpha4"
 	"seedhammer.com/image/rgb565"
+)
+
+var (
+	artifacts = flag.Bool("artifacts", false, "dump mismatching images")
+	update    = flag.Bool("update", false, "update golden images")
 )
 
 func TestDrawAlphaUniformOver(t *testing.T) {
@@ -33,4 +46,151 @@ func TestDrawAlphaUniformOver(t *testing.T) {
 	if !slices.Equal(dst.Pix, want) {
 		t.Fatalf("image:\n%.16b\nexpected:\n%.16b\n", dst.Pix, want)
 	}
+}
+
+func TestRoundedRect(t *testing.T) {
+	testGolden(t, "rounded-rect", 100, 60, func(ctx Ctx) {
+		outer := image.Rect(11, 12, 41, 52)
+		inner := image.Rect(21, 22, 31, 42)
+		ColorOp(ctx, argb(0xde11adbe))
+
+		RoundedRect(ctx, outer, 10)
+		ColorOp(ctx, argb(0xf03530aa))
+
+		RoundedRect(ctx, inner, 4)
+		ColorOp(ctx, argb(0xffffffff))
+
+		Offset(ctx, image.Pt(50, 0))
+		RoundedOutline(ctx, outer, 10, 1)
+		ColorOp(ctx, argb(0xf1a2040a))
+
+		Offset(ctx, image.Pt(50, 0))
+		RoundedOutline(ctx, inner, 4, 1)
+		ColorOp(ctx, argb(0xff000000))
+
+		Offset(ctx, image.Pt(25, 0))
+		ClipOp(image.Rect(22, 23, 30, 41)).Add(ctx)
+		RoundedRect(ctx, inner, 4)
+		ColorOp(ctx, argb(0xff00ffff))
+	})
+}
+
+func TestClip(t *testing.T) {
+	testGolden(t, "alpha-mask", 100, 60, func(ctx Ctx) {
+		r := image.Rect(1, 2, 31, 42)
+		ColorOp(ctx, argb(0xde11adbe))
+
+		m := ctx.Begin()
+		ClipOp(r).Add(m)
+		ColorOp(m, argb(0xf03530aa))
+		c := ctx.End()
+		Offset(ctx, image.Pt(10, 10))
+		c.Add(ctx)
+
+		m = ctx.Begin()
+		Offset(ctx, image.Pt(10, 10))
+		ClipOp(r).Add(m)
+		ColorOp(m, argb(0xf1a2040a))
+		c = ctx.End()
+		Offset(ctx, image.Pt(50, 0))
+		c.Add(ctx)
+	})
+}
+
+func TestImageMask(t *testing.T) {
+	testGolden(t, "image-mask", 80, 50, func(ctx Ctx) {
+		ColorOp(ctx, argb(0xde11adbe))
+
+		Offset(ctx, image.Pt(10, 10))
+		ImageOp(ctx, assets.IconDiscard, true)
+		ColorOp(ctx, argb(0xffdfcf0f))
+
+		fnt := poppins.Bold25
+		m := fnt.Metrics()
+		Offset(ctx, image.Pt(50, m.Ascent.Round()+10))
+		GlyphOp(ctx, fnt, 'R')
+		ColorOp(ctx, argb(0xffdfcf0f))
+	})
+}
+
+func TestPaletted(t *testing.T) {
+	testGolden(t, "paletted", 150, 150, func(ctx Ctx) {
+		ColorOp(ctx, argb(0xde11adbe))
+
+		Offset(ctx, image.Pt(10, 10))
+		ImageOp(ctx, assets.Hammer, false)
+	})
+}
+
+func testGolden(t *testing.T, name string, w, h int, f func(ctx Ctx)) {
+	t.Helper()
+
+	var ops Ops
+	ctx := ops.Context()
+
+	f(ctx)
+
+	dr := image.Rect(0, 0, w, h)
+	fb := rgb565.New(dr)
+	mask := image.NewAlpha(dr)
+	ops.Draw(fb, mask)
+	if err := os.MkdirAll("testdata", 0o770); err != nil {
+		t.Fatal(err)
+	}
+	path := filepath.Join("testdata", name+".png")
+	buf := new(bytes.Buffer)
+	if err := png.Encode(buf, fb); err != nil {
+		t.Fatal(err)
+	}
+	imgBytes := buf.Bytes()
+	if *update {
+		if err := os.WriteFile(path, imgBytes, 0o600); err != nil {
+			t.Fatal(err)
+		}
+	}
+	gf, err := os.Open(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer gf.Close()
+	golden, err := png.Decode(gf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := png.Decode(bytes.NewReader(imgBytes))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := compareImages(got, golden); err != nil {
+		if *artifacts {
+			if err := os.WriteFile(name+"-mismatch.png", imgBytes, 0o660); err != nil {
+				t.Error(err)
+			}
+		}
+		t.Error(err)
+	}
+}
+
+func compareImages(img, golden image.Image) error {
+	b1, b2 := img.Bounds(), golden.Bounds()
+	if b1 != b2 {
+		return fmt.Errorf("got dimensions %v, expected %v", b1, b2)
+	}
+	mismatches := 0
+	for y := b1.Min.Y; y < b1.Max.Y; y++ {
+		for x := b1.Min.X; x < b1.Max.X; x++ {
+			p1, p2 := img.At(x, y), golden.At(x, y)
+			if p1 != p2 {
+				mismatches++
+			}
+		}
+	}
+	if mismatches > 0 {
+		return fmt.Errorf("%d pixels mismatch", mismatches)
+	}
+	return nil
+}
+
+func argb(c uint32) color.RGBA {
+	return color.RGBA{A: uint8(c >> 24), R: uint8(c >> 16), G: uint8(c >> 8), B: uint8(c)}
 }
